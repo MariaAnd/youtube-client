320285e7e77d1a5b1d2780178ac956505392b522
Maria Andreyeva <maria.andreyeva@nl.abnamro.com>
2020-08-05 20:05:04 +0200

    pipe is created;

--- src/app/pipes/sort-by-date/sort-by-date.pipe.ts
+++ src/app/pipes/sort-by-date/sort-by-date.pipe.ts
@@ -7,25 +7,8 @@ import {SearchItem} from '../../models/search-item.model';
       })
 export class SortByDatePipe implements PipeTransform {
   public transform(array: SearchItem[], filterType: string): SearchItem[] {
-    // if (array && filterType === 'date') {
-    //   function bubbleSort(inputArr: SearchItem[]): SearchItem[] {
-    //     let len: number = inputArr.length - 1;
-    //     let swapped: boolean;
-    //     do {
-    //       swapped = false;
-    //       for (let i = 0; i < len; i++) {
-    //         if (inputArr[i].snippet.publishedAt > inputArr[i + 1].snippet.publishedAt) {
-    //           let tmp: SearchItem = inputArr[i];
-    //           inputArr[i] = inputArr[i + 1];
-    //           inputArr[i + 1] = tmp;
-    //           swapped = true;
-    //         }
-    //       }
-    //     } while (swapped);
-    //     return inputArr;
-    //   };
-    //   bubbleSort(array);
-    // }
+    if (array && filterType === 'date') {
+    }
     if (array && filterType === 'views') {
       let len: number = array.length - 1;
       let swapped: boolean;
