1d4ef9c4c04c6696a9455df3012e5d74a6b6cd86
Maria Andreyeva <maria.andreyeva@nl.abnamro.com>
2020-08-29 18:40:17 +0200

    typing functionality is added;

--- src/app/core/components/search-bar/search-bar.component.ts
+++ src/app/core/components/search-bar/search-bar.component.ts
@@ -1,21 +1,49 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnDestroy,
+  Output,
+  ViewChild
+} from '@angular/core';
+import {fromEvent, Observable, Subscription} from 'rxjs';
+import {debounceTime, distinctUntilChanged, filter, map, startWith, tap} from 'rxjs/operators';
+import {MIN_SYMBOLS, DEBOUNCE_TIME} from './const.component';
 
 @Component({
              selector: 'app-search-bar',
              templateUrl: './search-bar.component.html',
              styleUrls: ['./search-bar.component.scss']
            })
-export class SearchBarComponent implements OnInit {
+export class SearchBarComponent implements AfterViewInit, OnDestroy {
+  private subscription: Subscription;
   @Output() public searchSubmitted: EventEmitter<string> = new EventEmitter<string>();
+
+  @ViewChild('searchInput', {static: false}) public input: ElementRef;
 
   constructor() {
   }
 
-  public ngOnInit(): void {
+  public ngAfterViewInit(): void {
+    const searchTerm$: Observable<string> = fromEvent<KeyboardEvent>(this.input.nativeElement, 'keyup')
+      .pipe(
+        map(event => (<HTMLInputElement>event.target).value),
+        filter(term => term.length > MIN_SYMBOLS),
+        startWith(''),
+        debounceTime(DEBOUNCE_TIME),
+        distinctUntilChanged()
+      );
+    this.subscription = searchTerm$
+      .subscribe(
+        searchTerm => {
+          this.searchSubmitted.emit(searchTerm);
+        }
+      );
   }
 
-  public getSearchResults(): void {
-    this.searchSubmitted.emit('term');
+  public ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
